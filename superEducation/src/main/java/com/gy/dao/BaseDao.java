fb2154d0b4b7493af65896e7bb6e98d8e6eeb0ac
lgy <1354449188@qq.com>
2017-03-05 09:31:50 +0800

    三期项目

---
+++ superEducation/src/main/java/com/gy/dao/BaseDao.java
@@ -0,0 +1,36 @@
+package com.gy.dao;
+
+import java.util.List;
+import java.util.Map;
+
+
+//这是dao层的接口
+public interface BaseDao<T> {
+
+	public List<T> findAll(T t,String sqlId);
+	
+	public List<T> findAll(T t,Map map,String sqlId);
+	
+	//添加
+	public int add(T t,String sqlId);
+	
+	public int add(T t,Map map,String sqlId);
+	//删除
+	public int delete(T t,String sqlId);
+	
+	public int deleteMany(T t,List list,String sqlId);
+	
+	public int delete(T t,Map map,String sqlId);
+	//修改
+	public int update(T t, String sqlId);
+	
+	public int update(T t,Map map,String sqlId);
+	
+	public int updateMany(T t,List list,String sqlId);
+	
+	//聚合查询
+	public double fiondFunc(T t,String sqlId);
+	
+	public double fiondFunc(T t,Map map,String sqlId);
+	//条件查询
+}
